0fab541653b01019d50e14651940ccac3eedd6b7
Les Moffat <les@lesmoffat.co.uk>
2018-10-19 16:30:42 +0100

    delete all django files

--- infodistrictes/frontend/models.py
+++
@@ -1,4 +0,0 @@
-import html
-import json
-
-from django.db import models
--- infodistrictes/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-# from django.urls import path
-#
-# from deltebre.frontend import views
-#
-#
-# urlpatterns = [
-#     # Sample: path('<int:question_id>/', views.detail, name='detail'),
-#     # path('', views.HomeView.as_view(), name='home'),
-# ]
